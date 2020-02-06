096931cbed154a6786d52c16d9fe3f36bde1f269
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-02-06 09:09:37 +0300

    Заполнение большой фотографии.

--- js/main.js
+++ js/main.js
@@ -19,9 +19,15 @@ var COMMENTS_TEXTS = [
 var AUTHORS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
 var pictures = document.querySelector('.pictures');
+var bigPicture = document.querySelector('.big-picture');
+var bigPictureImg = bigPicture.querySelector('.big-picture__img img');
+var bigPictureLikesCount = bigPicture.querySelector('.likes-count');
+var bigPictureDescription = bigPicture.querySelector('.social__caption');
+var bigPictureCommentsCount = bigPicture.querySelector('.comments-count');
+var bigPictureSocialComments = bigPicture.querySelector('.social__comments');
 
-//  Шаблон фотографии
 var photoTemplate = document.querySelector('#picture').content;
+var socialCommentTemplate = document.querySelector('#social-comment').content;
 
 //  Функция нахождения случайного числа
 var randomNumber = function (num) {
@@ -78,3 +84,39 @@ var renderPhotos = function (photosArray) {
 
 var photos = photosCreate(PHOTOS_COUNT);
 pictures.appendChild(renderPhotos(photos));
+window.console.log(photos);
+
+
+/*  Большая фотография  */
+
+//  Функция отрисовки одного комментария
+var renderSocialComment = function (comment) {
+  var commentElement = socialCommentTemplate.cloneNode(true);
+  commentElement.querySelector('.social__picture').src = comment.avatar;
+  commentElement.querySelector('.social__picture').alt = comment.name;
+  commentElement.querySelector('.social__text').textContent = comment.message;
+  return commentElement;
+};
+
+//  Функция отрисовки всех комментариев
+var renderSocialComments = function (commentsArray) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < commentsArray.length; i++) {
+    fragment.appendChild(renderSocialComment(commentsArray[i]));
+  }
+  return fragment;
+};
+
+//  Функция отрисовки большой фотографии
+/* var renderBigPhoto = function (bigPhoto) {
+
+}; */
+
+bigPictureImg.src = photos[0].url;
+bigPictureLikesCount.textContent = photos[0].likes;
+bigPictureCommentsCount.textContent = photos[0].comments.length;
+bigPictureDescription.textContent = photos[0].description;
+bigPictureSocialComments.innerHTML = '';
+bigPictureSocialComments.appendChild(renderSocialComments(photos[0].comments));
+
+bigPicture.classList.remove('hidden');
